871f4f1c65283a8946688bbb92af044520dde090
Amzat19 <kunmiamzat1@gmail.com>
2024-04-24 17:14:19 +0100

    Updated resume and experience

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,58 +1,89 @@
-import Amzat from "../assets/Amzat.jpg"
-import phonelogo from "../assets/phone-call.png"
-import phonelogowhite from "../assets/phonelogowhite.webp"
-import { ReactComponent as Mail } from "../assets/mail-logo.svg"
+import Amzat from "../assets/Amzat.jpg";
+import phonelogo from "../assets/phone-call.png";
+import phonelogowhite from "../assets/phonelogowhite.webp";
+import { ReactComponent as Mail } from "../assets/mail-logo.svg";
 import { ReactComponent as LinkedInLogo } from "../assets/linkedin-logo.svg";
 import { ReactComponent as GithubLogo } from "../assets/github-logo.svg";
 
 const AboutMe = ({ theme }) => {
   return (
-    <article id="contact" className="mx-auto pb-6 bg-[#FFFFFF] dark:bg-slate-800 rounded-xl shadow-lg md:flex md:justify-between w-[80vw] dark:text-[#FFFFFF]">
+    <article
+      id="contact"
+      className="mx-auto pb-6 bg-[#FFFFFF] dark:bg-slate-800 rounded-xl shadow-lg md:flex md:justify-between w-[80vw] dark:text-[#FFFFFF]"
+    >
       <div className="md:h-full h-[400px] pt-8">
-        <img src={Amzat} alt="Amzat Sheriff" className="mx-auto w-4/5 h-[90%] md:w-40 md:h-4/5 md:m-8 rounded-xl" />
+        <img
+          src={Amzat}
+          alt="Amzat Sheriff"
+          className="mx-auto w-4/5 h-[90%] md:w-40 md:h-4/5 md:m-8 rounded-xl"
+        />
       </div>
-      <div className="h-4/5 w-3/5 mx-8 mt-8 lg:w-4/5">
+      <div className="h-4/5 w-4/5 mx-8 mt-8 lg:w-4/5">
         <div className="grid gap-4 md:flex md:justify-between">
           <div>
-            <h2 className="text-2xl font-semibold text-[#4F4F4F] dark:text-[#FFFFFF]">Amzat Sheriff</h2>
-            <p className="text-[#828282] dark:text-[#FFFFFF]">Front-end Developer</p>
+            <h2 className="text-2xl font-semibold text-[#4F4F4F] dark:text-[#FFFFFF]">
+              Amzat Sheriff
+            </h2>
+            <p className="text-[#828282] dark:text-[#FFFFFF]">
+              Front-end Developer
+            </p>
           </div>
           <div>
             <span className="flex gap-2 mb-1">
               <Mail className="dark:fill" />
-              <p className="text-[#4F4F4F] dark:text-[#FFFFFF]">kunmiamzat1@gmail.com</p>
+              <p className="text-[#4F4F4F] dark:text-[#FFFFFF]">
+                kunmiamzat1@gmail.com
+              </p>
             </span>
             <span className="flex gap-2">
-              {theme === "light" ? <img src={phonelogo} alt="phone logo" className="w-6 h-6" /> : <img src={phonelogowhite} alt="phone logo" className="w-6 h-6" />}
-              <p className="text-[#4F4F4F] dark:text-[#FFFFFF]">+2348120060057</p>
+              {theme === "light" ? (
+                <img src={phonelogo} alt="phone logo" className="w-6 h-6" />
+              ) : (
+                <img
+                  src={phonelogowhite}
+                  alt="phone logo"
+                  className="w-6 h-6"
+                />
+              )}
+              <p className="text-[#4F4F4F] dark:text-[#FFFFFF]">
+                +2348120060057
+              </p>
             </span>
           </div>
         </div>
-        <div className="my-6 w-[50vw]">
+        <div className="my-6 w-full">
           <p className="text-[#828282] dark:text-[#FFFFFF]">
-            Passionate Frontend developer with a proven track record of creating stunning and responsive web experiences. I have honed my skills in HTML, CSS, JavaScript, and various frontend frameworks, including React and Vue. I excel at collaborating with cross-functional teams to deliver innovative solutions that delight users and drive business growth. Whether it's building from scratch or revamping existing projects, I am always eager to take on new challenges and bring my creativity to the table. Let's work together to create something amazing!
+            Passionate Frontend developer with a proven track record of creating
+            stunning and responsive web experiences. I have honed my skills in
+            HTML, CSS, JavaScript, and various frontend frameworks, including
+            React and Vue. I excel at collaborating with cross-functional teams
+            to deliver innovative solutions that delight users and drive
+            business growth. Whether it's building from scratch or revamping
+            existing projects, I am always eager to take on new challenges and
+            bring my creativity to the table. Let's work together to create
+            something amazing!
           </p>
         </div>
         <div className="flex flex-wrap gap-4 md:justify-around md:gap-0">
-          <a href='mailto:kunmiamzat1@gmail.com'>
-            <button className='flex rounded-sm border-2 h-12 px-6 py-3 gap-1 border-gradient-mail border-solid border-transparent dark:darkmode-border-gradient-mail'>
+          <a href="mailto:kunmiamzat1@gmail.com">
+            <button className="flex rounded-sm border-2 h-12 px-6 py-3 gap-1 border-gradient-mail border-solid border-transparent dark:darkmode-border-gradient-mail">
               <Mail className="dark:fill" /> Mail
             </button>
           </a>
-          <a href='https://www.linkedin.com/in/kunmi-amzat'>
-            <button className='flex rounded-sm border-2 h-12 px-6 py-3 gap-1 border-gradient-linkedin border-solid border-transparent dark:darkmode-border-gradient-linkedin'>
+          <a href="https://www.linkedin.com/in/kunmi-amzat">
+            <button className="flex rounded-sm border-2 h-12 px-6 py-3 gap-1 border-gradient-linkedin border-solid border-transparent dark:darkmode-border-gradient-linkedin">
               <LinkedInLogo className="dark:fill" /> LinkedIn
             </button>
           </a>
-          <a href='https://github.com/Amzat19'>
-            <button className='flex rounded-sm border-2 h-12 px-4 py-3 gap-1 border-gradient-github border-solid border-transparent dark:darkmode-border-gradient-github'>
+          <a href="https://github.com/Amzat19">
+            <button className="flex rounded-sm border-2 h-12 px-4 py-3 gap-1 border-gradient-github border-solid border-transparent dark:darkmode-border-gradient-github">
               <GithubLogo className="dark:fill" /> GitHub
             </button>
           </a>
         </div>
       </div>
     </article>
-  )
-}
+  );
+};
 
 export default AboutMe;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,17 +1,20 @@
-import gaming from "../assets/gaming.jpg"
-import biking from "../assets/biking.jpg"
-import reading from "../assets/reading.jpg"
-import scai from "../assets/scai.jpg"
-import hng from "../assets/hng.png"
-import pentria from "../assets/pentria.jpg"
-import pharmalink from "../assets/pharmalink.png"
+import gaming from "../assets/gaming.jpg";
+import biking from "../assets/biking.jpg";
+import reading from "../assets/reading.jpg";
+import scai from "../assets/scai.jpg";
+import hng from "../assets/hng.png";
+import pentria from "../assets/pentria.jpg";
+import pharmalink from "../assets/pharmalink.png";
+import blucera from "../assets/blucera.png";
 
 const Experience = () => {
   return (
     <section className="mt-8 mx-auto w-[80vw]  sm:flex sm:gap-4">
       <div className="flex gap-4 sm:grid">
         <article className="bg-[#FFFFFF] dark:bg-slate-800 rounded-xl p-8 w-2/4 h-max shadow-lg sm:w-full">
-          <h2 className="text-[#4F4F4F] dark:text-[#FFFFFF] font-bold text-lg uppercase mb-2">Front end Skills</h2>
+          <h2 className="text-[#4F4F4F] dark:text-[#FFFFFF] font-bold text-lg uppercase mb-2">
+            Front end Skills
+          </h2>
           <ul className="text-[#4F4F4F] dark:text-[#FFFFFF] font-semibold text-sm">
             <li>React</li>
             <li>Vue</li>
@@ -29,63 +32,145 @@ const Experience = () => {
           </ul>
         </article>
         <article className="bg-[#FFFFFF] dark:bg-slate-800 rounded-xl p-8 w-2/4 shadow-lg sm:w-full">
-          <h2 className="text-[#4F4F4F] dark:text-[#FFFFFF] font-bold text-lg uppercase">Hobbies</h2>
+          <h2 className="text-[#4F4F4F] dark:text-[#FFFFFF] font-bold text-lg uppercase">
+            Hobbies
+          </h2>
           <div className="flex flex-wrap gap-2">
             <div className="mt-4">
-              <img src={gaming} alt="A Gamer with a gaming pad" className="rounded-md w-full" />
-              <h3 className="text-[#333333] dark:text-[#FFFFFF] font-semibold my-2">Gaming</h3>
+              <img
+                src={gaming}
+                alt="A Gamer with a gaming pad"
+                className="rounded-md w-full"
+              />
+              <h3 className="text-[#333333] dark:text-[#FFFFFF] font-semibold my-2">
+                Gaming
+              </h3>
             </div>
             <div className="mt-4">
-              <img src={biking} alt="A Gamer with a gaming pad" className="rounded-md w-full max-w-[192px]" />
-              <h3 className="text-[#333333] dark:text-[#FFFFFF] font-semibold my-2">Biking</h3>
+              <img
+                src={biking}
+                alt="A Gamer with a gaming pad"
+                className="rounded-md w-full max-w-[192px]"
+              />
+              <h3 className="text-[#333333] dark:text-[#FFFFFF] font-semibold my-2">
+                Biking
+              </h3>
             </div>
             <div className="mt-4">
-              <img src={reading} alt="A Gamer with a gaming pad" className="rounded-md w-full max-w-[192px]" />
-              <h3 className="text-[#333333] dark:text-[#FFFFFF] font-semibold my-2">Reading</h3>
+              <img
+                src={reading}
+                alt="A Gamer with a gaming pad"
+                className="rounded-md w-full max-w-[192px]"
+              />
+              <h3 className="text-[#333333] dark:text-[#FFFFFF] font-semibold my-2">
+                Reading
+              </h3>
             </div>
           </div>
-
         </article>
       </div>
       <div className="sm:w-[500px] sm:self-start ">
         <article className="bg-[#FFFFFF] dark:bg-slate-800 rounded-xl p-8 mt-8 shadow-lg sm:mt-0">
-          <h2 className="text-[#4F4F4F] dark:text-[#FFFFFF] uppercase font-bold">Experiences</h2>
+          <h2 className="text-[#4F4F4F] dark:text-[#FFFFFF] uppercase font-bold">
+            Experiences
+          </h2>
           <div className="flex gap-4 mt-4">
-            <img src={pharmalink} alt="pharmalink logo" className="w-2/6 h-4/6 self-center" />
+            <img
+              src={blucera}
+              alt="pharmalink logo"
+              className="w-2/6 h-4/6 self-center"
+            />
             <div className="self-center">
-              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">April 2023 - Present</p>
-              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">FrontEnd Engineer</p>
+              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">
+                October 2023 - Present
+              </p>
+              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">
+                FrontEnd Engineer
+              </p>
             </div>
           </div>
-          <p className="text-[#828282] dark:text-[#FFFFFF] my-4">Collaborated with cross-functional teams to design and develop intuitive and visually appealing desktop interfaces that enhance patient engagement and improve healthcare outcomes.</p>
+          <p className="text-[#828282] dark:text-[#FFFFFF] my-4">
+            Partnered with cross-functional teams to design and develop
+            intuitive and visually appealing desktop interfaces for a financial
+            services platform, supporting features like training portals,
+            business tools, marketplace integration and more.
+          </p>
           <div className="flex gap-4 mt-4">
-            <img src={pentria} alt="hng internship logo" className="w-2/6 h-4/6 self-center" />
+            <img
+              src={pharmalink}
+              alt="pharmalink logo"
+              className="w-2/6 h-4/6 self-center"
+            />
             <div className="self-center">
-              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">Jan 2023 - Mar 2023</p>
-              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">FrontEnd Developer</p>
+              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">
+                April 2023 - Present
+              </p>
+              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">
+                FrontEnd Engineer
+              </p>
             </div>
           </div>
-          <p className="text-[#828282] dark:text-[#FFFFFF] my-4">Collaborated with a team of designers and developers to create a user-friendly interface and seamless client to server interactions for an event booking application.</p>
+          <p className="text-[#828282] dark:text-[#FFFFFF] my-4">
+            Collaborated with cross-functional teams to design and develop
+            intuitive and visually appealing desktop interfaces that enhance
+            patient engagement and improve healthcare outcomes.
+          </p>
+          <div className="flex gap-4 mt-4">
+            <img
+              src={pentria}
+              alt="hng internship logo"
+              className="w-2/6 h-4/6 self-center"
+            />
+            <div className="self-center">
+              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">
+                Jan 2023 - Mar 2023
+              </p>
+              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">
+                FrontEnd Developer
+              </p>
+            </div>
+          </div>
+          <p className="text-[#828282] dark:text-[#FFFFFF] my-4">
+            Collaborated with a team of designers and developers to create a
+            user-friendly interface and seamless client to server interactions
+            for an event booking application.
+          </p>
           <div className="flex gap-4 mt-4">
             <img src={hng} alt="hng internship logo" className="w-2/6 h-4/5" />
             <div className="self-center">
-              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">Oct 2022 - Dec 2022</p>
-              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">FrontEnd Developer Intern</p>
+              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">
+                Oct 2022 - Dec 2022
+              </p>
+              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">
+                FrontEnd Developer Intern
+              </p>
             </div>
           </div>
-          <p className="text-[#828282] dark:text-[#FFFFFF] my-4">Contributed to build an hangout scheduling platform. I collaborated with the team to implement features, optimize performance, and resolve frontend issues.</p>
+          <p className="text-[#828282] dark:text-[#FFFFFF] my-4">
+            Contributed to build an hangout scheduling platform. I collaborated
+            with the team to implement features, optimize performance, and
+            resolve frontend issues.
+          </p>
           <div className="flex gap-4">
             <img src={scai} alt="Smart Finance Logo" className="w-2/5 h-4/5" />
             <div className="self-center">
-              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">Aug 2022 - Oct 2022</p>
-              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">FullStack Developer</p>
+              <p className="text-[#828282] dark:text-[#FFFFFF] text-sm mb-2">
+                Aug 2022 - Oct 2022
+              </p>
+              <p className="text-[#333333] dark:text-[#FFFFFF] font-semibold mb-2">
+                FullStack Developer
+              </p>
             </div>
           </div>
-          <p className="text-[#828282] dark:text-[#FFFFFF] mt-4">Worked in a team of 3 engineers building a full stack cloud ready web application. Contributed to the development of the backend and collaborated on the frontend.</p>
+          <p className="text-[#828282] dark:text-[#FFFFFF] mt-4">
+            Worked in a team of 3 engineers building a full stack cloud ready
+            web application. Contributed to the development of the backend and
+            collaborated on the frontend.
+          </p>
         </article>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Experience;
