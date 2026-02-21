a36ad05eb89e00d9dc53bc1380e386f94e6e23ca
Amzat19 <kunmiamzat1@gmail.com>
2026-02-21 15:43:13 +0100

    Updated resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,7 +34,7 @@ const NavBar = ({ navState, toggleNav, scrollToSection }) => {
             <a
               rel="noreferrer"
               target="_blank"
-              href="https://drive.google.com/file/d/1bA-kj3hpJQnIoJED2fzqdLmFWsRCuFnf/view?usp=drivesdk"
+              href="https://drive.google.com/file/d/1H3yMgEw5CJ_M0U3ngTXmX_eZdeWAMXKT/view?usp=sharing"
             >
               Resume
             </a>
@@ -102,7 +102,7 @@ const Header = ({ handleSwitchTheme }) => {
                 <a
                   rel="noreferrer"
                   target="_blank"
-                  href="https://drive.google.com/file/d/1bA-kj3hpJQnIoJED2fzqdLmFWsRCuFnf/view?usp=drivesdk"
+                  href="https://drive.google.com/file/d/1H3yMgEw5CJ_M0U3ngTXmX_eZdeWAMXKT/view?usp=sharing"
                 >
                   Resume
                 </a>
